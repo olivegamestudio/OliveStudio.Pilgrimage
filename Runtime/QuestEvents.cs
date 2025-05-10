d902fc026243d853a80253d6f07e959094ef2974
Simon Jefferies <simon@olivegame.studio>
2025-05-10 22:55:41 +0100

    Added QuestEvents/System.

---
+++ Runtime/QuestEvents.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public static class QuestEvents
+{
+    /// <summary>
+    /// Event triggered when a quest is started.
+    /// </summary>
+    public static event EventHandler<QuestStartedEventArgs> QuestStarted = delegate { };
+
+    /// <summary>
+    /// Event triggered when a quest is completed.
+    /// </summary>
+    public static event EventHandler<QuestCompletedEventArgs> QuestCompleted = delegate { };
+
+    /// <summary>
+    /// Event triggered when the player reaches a point of interest.
+    /// </summary>
+
+    public static event EventHandler<PointOfInterestEventArgs> ReachedPointOfInterest = delegate { };
+
+    /// <summary>
+    /// Invokes the QuestStarted event.
+    /// </summary>
+    /// <param name="quest">The quest that was started.</param>
+    public static void NotifyQuestStarted(Quest quest) 
+        => QuestStarted.Invoke(null, new QuestStartedEventArgs(quest));
+
+    /// <summary>
+    /// Invokes the QuestCompleted event.
+    /// </summary>
+    /// <param name="quest">The quest that was completed.</param>
+    public static void OnQuestCompleted(Quest quest) 
+        => QuestCompleted.Invoke(null, new QuestCompletedEventArgs(quest));
+
+    /// <summary>
+    /// Invokes the reached point of interest event.
+    /// </summary>
+    public static void OnReachedPointOfInterest()
+        => ReachedPointOfInterest.Invoke(null, new PointOfInterestEventArgs());
+}
---
+++ Runtime/QuestSystem.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class QuestSystem : MonoBehaviour
+{
+    /// <summary>
+    /// The array of quests managed by the quest system.
+    /// </summary>
+    public Quest[] Quests = Array.Empty<Quest>();
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// Initializes the quest system and subscribes to events.
+    /// </summary>
+    void Start()
+    {
+        Quests = gameObject.GetComponentsInChildren<Quest>();
+    }
+
+    /// <summary>
+    /// Subscribes to the EnemyDestroyed and QuestCompleted events.
+    /// </summary>
+    void OnEnable()
+    {
+        Game.QuestCompleted += OnQuestCompleted;
+    }
+
+    /// <summary>
+    /// Unsubscribes from the EnemyDestroyed and QuestCompleted events.
+    /// </summary>
+    void OnDisable()
+    {
+        Game.QuestCompleted -= OnQuestCompleted;
+    }
+
+    /// <summary>
+    /// Gets the quest with the specified identifier.
+    /// </summary>
+    /// <param name="questId">The identifier of the quest.</param>
+    /// <returns>The quest with the specified identifier, or null if not found.</returns>
+    public Quest GetQuest(int questId)
+        => Quests.FirstOrDefault(it => it.Id == questId);
+
+    /// <summary>
+    /// Check a quest can be completed.
+    /// </summary>
+    void OnCheckQuestCompletion()
+    {
+        foreach (Quest quest in Quests)
+        {
+            if (quest.CanAutoComplete && quest.IsComplete)
+            {
+                Game.OnQuestCompleted(quest);
+            }
+        }
+    }
+}
